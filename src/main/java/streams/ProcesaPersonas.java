bb343e1c7b6d6831fae9fed444bc42baf103982c
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2017-11-25 18:49:10 +0100

    Funciona con la última versión del generador de datos, la 0.0.5

--- src/main/java/streams/ProcesaPersonas.java
+++ src/main/java/streams/ProcesaPersonas.java
@@ -6,22 +6,14 @@ public class ProcesaPersonas {
     public static void main(String[] args) {
         ProcesaPersonas procesaPersonas = new ProcesaPersonas();
         procesaPersonas.personasEnProvincia("Castellón");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     private void personasEnProvincia(String provincia) {
-        List<Persona> personas = Persona.aleatorias(100);
+        List<Persona> personas = Persona.aleatorias(1000);
 
         personas.stream()
                 .filter(p -> provincia.equalsIgnoreCase(p.getProvincia()))
                 .findFirst()
                 .ifPresent(System.out::println);
-
-        System.out.println(personas);
-
     }
 }
